4131cc30b2766738452c1bd8ecf9dd5393536bb4
Cat Fortman <catherine.fortman@gmail.com>
2020-02-25 11:40:54 -0400

    Home and register components
    Home and register components added

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
+  registerMode = false;
   values: any;
 
   constructor(private http: HttpClient) { }
@@ -16,7 +17,15 @@ export class ValueComponent implements OnInit {
     this.getValues();
   }
 
-  getValues(){
+  register() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+
+  getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
     }, error => {
