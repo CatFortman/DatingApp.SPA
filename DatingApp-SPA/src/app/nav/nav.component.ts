79a28c538072ec5d572cf70c04e1427ef81e5d52
Cat Fortman <catherine.fortman@gmail.com>
2020-02-28 11:30:40 -0400

    Function dropdown logout

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
 export class NavComponent implements OnInit {
 
   model: any = {};
+
 
   constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
@@ -33,5 +36,6 @@ export class NavComponent implements OnInit {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
   }
+
 
 }
