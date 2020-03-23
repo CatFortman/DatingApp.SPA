238a42b58e407bee341be4cb8a910295b1c2a566
Cat Fortman <catherine.fortman@gmail.com>
2020-03-23 15:47:53 -0300

    IActionFilter log user activity

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { FileUploadModule } from 'ng2-file-upload';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -53,7 +54,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
       MemberDetailComponent,
       MemberEditComponent,
       PhotoEditorComponent,
-      EncodeUriPipe
+      EncodeUriPipe,
+      TimeAgoPipe
    ],
    imports: [
       BrowserModule,
